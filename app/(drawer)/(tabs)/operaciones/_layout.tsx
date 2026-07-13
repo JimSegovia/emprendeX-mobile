834e95d2bef741f1c3434106eb6763ae84cacc5f
Joshua Chavez <jnchavezc@gmail.com>
2026-07-12 23:59:33 -0500

    feat(ventas): agregar botón editar en detalle y registrar ruta en layout

--- app/(drawer)/(tabs)/operaciones/_layout.tsx
+++ app/(drawer)/(tabs)/operaciones/_layout.tsx
@@ -16,6 +16,7 @@ export default function OperacionesLayout() {
       <Stack.Screen name="index" />
       <Stack.Screen name="nueva" />
       <Stack.Screen name="[id]" />
+      <Stack.Screen name="editar-cotizacion" />
     </Stack>
   );
 }
